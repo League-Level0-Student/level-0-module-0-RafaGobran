80dfaf45fe0838e735965441d1c7972cf16da1a9
league <hailinday@gmail.com>
2024-04-11 16:45:31 -0700

    Today i made a crazy pentagon

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -11,23 +11,23 @@ public class PentagonCrazy {
 	
 	public static void main(String[] args) {
 		
-		//  Create a new Robot
-
+		//  Create a new Rob
+		Robot robtop = new Robot ();
 		//  Put the robot's pen down
-
+robtop.penDown();
 		//  SPEED. Make the robot go at maximum speed (100)
-
+robtop.setSpeed(100);
 		//  COLOR. Set the pen to a color that you like for the shape
-
+robtop.setPenColor(130,0, 0);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape will have.
 		//  				(Hint: its called PentagonCrazy)
-
+int sides = 11;
 		//  TURN ANGLE. Make another int variable for the angle the robot must turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has to get the angle.
-		
+		int acute = 360/sides;
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-
+int count =0;
 		//  LOOP. Start a while loop to repeat the DRAW, TURN, and INCREASE COUNT code 200 times. 
 		
 				//  DRAW.  Make the robot move the amount in your count variable
@@ -35,11 +35,16 @@ public class PentagonCrazy {
 				//  TURN.  Turn the robot the amount in your angle variable + 1
 
 				//  INCREASE COUNT. Increase the count by 1
+		while (count<200)
+		{robtop.move(1 * count);
+		robtop.turn(acute+1);
+		count += 1;
+		}
 		
 		//  End the while loop here
 
 		//  Now hide the robot so you can see the pattern it has drawn.
-		
+		robtop.hide();
 		//  Run the program. 
 		//  Check the pattern against the picture in the recipe. If it matches, you are done!
 	
