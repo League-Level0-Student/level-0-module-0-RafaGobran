ecb0b97c7efb259edd49548895cdf3112f3790ed
league <hailinday@gmail.com>
2024-04-18 16:56:55 -0700

    today i did print and popups

---
+++ src/_03_print_and_popups/_2_madlibs/active.java
@@ -0,0 +1,5 @@
+package _03_print_and_popups._2_madlibs;
+
+public class active {
+
+}
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input = JOptionPane.showInputDialog("do you know how to write code");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		 if ("yes".equals(input)) {
+			 JOptionPane.showMessageDialog(null, "You Rule The World "); 
+		 }
 		// 3. Otherwise, tell them to sign up for classes at The League.
+		 if ("no".equals(input)) {
+			 JOptionPane.showMessageDialog(null, "you need help immediately! ");
+		 }
 
 	}
 }
-
